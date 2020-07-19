189d8abb885d77efb55cba935aa9a6aa221e9e99
nsoria1 <nicosoria18@gmail.com>
2020-07-19 19:54:08 -0300

    Adding Dash app files

---
+++ dashapp/app.py
@@ -0,0 +1,12 @@
+# Import required libraries
+import dash
+import dash_html_components as html
+
+# Initialise the app
+app = dash.Dash(__name__)
+
+# Define the app
+app.layout = html.Div()
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
