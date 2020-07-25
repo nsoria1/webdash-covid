eade2f5d2b1b5d8674f20b41521d1295652dee0d
nsoria1 <nicosoria18@gmail.com>
2020-07-24 21:00:02 -0300

    Subiendo nuevos cambios en notebook

--- dashapp/app.py
+++ dashapp/app.py
@@ -6,7 +6,14 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 
 # Define the app
-app.layout = html.Div()
+app.layout = html.Div(children=[
+                      html.Div(className='row',  # Define the row element
+                               children=[
+                                  html.Div(className='three columns div-user-controls'),  # Define the left element
+                                  html.Div(className='nine columns div-for-charts bg-grey')  # Define the right element
+                                  ])
+                                ])
+
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
