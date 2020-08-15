abd0bdd4873fc3a8d877c37159769734ad755f1c
nsoria1 <nicosoria18@gmail.com>
2020-08-15 11:46:45 -0300

    Se suben cambios al dashboard y se agrega nuevos indicadores

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
-from data.covid_get_raw_data import country_timeline_raw_data
-from get_dashboards import get_timeline_dash, create_dropdown
+from data.covid_get_raw_data import country_timeline_raw_data, country_raw_data
+from get_dashboards import get_timeline_dash, create_dropdown, create_indicators
 
 #--------------------------------------------------------------
 
@@ -18,11 +18,7 @@ server = app.server
 
 # Load data
 df_timeline = country_timeline_raw_data()
-
-#--------------------------------------------------------------
-
-# Create country timeline dashboard
-#fig1 = get_timeline_dash(df_timeline)
+df_country = country_raw_data()
 
 #--------------------------------------------------------------
 
@@ -34,9 +30,8 @@ app.layout = html.Div(
                  children=[
                     html.Div(className='three columns div-user-controls',
                              children=[
-                                 html.H2('DASH - STOCK PRICES'),
-                                 html.P('Visualising time series with Plotly - Dash.'),
-                                 html.P('Pick one or more stocks from the dropdown below.'),
+                                 html.H2('World Covid-19 Dashboard'),
+                                 html.P('Pick one country from the dropdown menu below'),
                                  html.Div(
                                      className='div-for-dropdown',
                                      children=[
@@ -45,29 +40,39 @@ app.layout = html.Div(
                                                       style={'backgroundColor': '#1E1E1E'}
                                                       ),
                                      ],
-                                     style={'color': '#1E1E1E'})
-                                ]
+                                     style={'color': '#1E1E1E'}),
+                                 html.P('Visualize confirmed, recovered and disease related to the Coronavirus per country.'),
+                                 html.P('Also find a chart that will show you a prediction numbers for the next day based on historical data.'),
+                                ],
                              ),
                     html.Div(className='nine columns div-for-charts bg-grey',
                              children=[
-                                 #dcc.Graph(id='fig1',config={'displayModeBar': False}, animate=True)
-                                 dcc.Graph(id='fig1', animate=True)
-                             ])
-                              ])
-        ]
-
-)
+                                 dcc.Graph(id='fig1', animate=True),
+                                 dcc.Graph(id='fig2', animate=True)
+                             ])#,
+                    #dcc.Graph(id='fig2', animate=True)
+                    ])
+                ]
+            )
 
 
 #--------------------------------------------------------------
 
 @app.callback(
     Output(component_id='fig1', component_property='figure'),
-    [Input(component_id='my_dropdown', component_property='value')]
-    )
+    [Input(component_id='my_dropdown', component_property='value')],
+)
 
 def update_graph(my_dropdown):
-  return get_timeline_dash(df_timeline, my_dropdown)
+    return get_timeline_dash(df_timeline, my_dropdown)
+
+@app.callback(
+    Output(component_id='fig2', component_property='figure'),
+    [Input(component_id='my_dropdown', component_property='value')]
+)
+
+def get_indicators(my_dropdown):
+    return create_indicators(df_country, my_dropdown)
 
 # Run the Dash app
 if __name__ == '__main__':
--- get_dashboards.py
+++ get_dashboards.py
@@ -18,13 +18,23 @@ def create_dropdown(data_list):
     return(dropdown_dict)
 
 #--------------------------------------------------------------
+# Plot scatter line related to info per country
 
 def get_timeline_dash(df, menu):
+    # Rename data frame columns
+    df.rename(
+        {"timeline.deaths": "deaths", 
+        "timeline.confirmed": "confirmed", 
+        "timeline.recovered": "recovered", 
+        "timeline.date": "date"},
+        axis='columns',
+        inplace=True)
+
     if not menu or menu=='All countries':
         dff = df
-        dff = dff.groupby('timeline.date').agg({'timeline.deaths': 'sum',
-                                                'timeline.confirmed': 'sum',
-                                                'timeline.recovered': 'sum'})
+        dff = dff.groupby('date').agg({'deaths': 'sum',
+                                                'confirmed': 'sum',
+                                                'recovered': 'sum'})
 
         barchar = px.line(
             dff,
@@ -33,22 +43,81 @@ def get_timeline_dash(df, menu):
             template='plotly_dark'
         )
 
+        barchar.update_yaxes(range=[dff['confirmed'].min(), dff['confirmed'].max()])
+
         return(barchar)
     
     else:
         dff = df[df['name']==menu]
+        #dff = df[(df['name'].isin(menu))] Line required for list selection in dropdown
         barchar = px.line(
             dff,
-            x=dff['timeline.date'],
+            x=dff['date'],
             y=[
-                dff['timeline.deaths'],
-                dff['timeline.confirmed'],
-                dff['timeline.recovered']
+                dff['deaths'],
+                dff['confirmed'],
+                dff['recovered']
                 ],
             template='plotly_dark')
         
-        barchar.update_yaxes(range=[3, 9])
+        barchar.update_yaxes(range=[dff['confirmed'].min(), dff['confirmed'].max()])
                 
         return(barchar)
 
 #--------------------------------------------------------------
+# Plot indicators
+def create_indicators(df, dropdown):
+    fig = go.Figure()
+    loop = 0
+
+    # Dictionary example to generate the indicators
+    if not dropdown:
+        data = {
+            'fig1': ['Critical Patients', 0],
+            'fig2': ['Death Rate', 0],
+            'fig3': ['Recovery Rate', 0],
+            'fig4': ['Cases per Million Population', 0]
+            }
+    else:
+        df2 = df[df['name']==dropdown]
+
+        df2 = df2.get(
+            ['latest_data.critical',
+            'latest_data.calculated.death_rate', 
+            'latest_data.calculated.recovery_rate', 
+            'latest_data.calculated.cases_per_million_population']
+            )
+        
+        df2.rename(
+            {"latest_data.critical": "critical", 
+            "latest_data.calculated.death_rate": "death_rate", 
+            "latest_data.calculated.recovery_rate": "recovery_rate", 
+            "latest_data.calculated.cases_per_million_population": "cases_per_million"},
+            axis='columns',
+            inplace=True)
+
+        data = {
+            'ind1': ['Critical Patients', df2['critical'].astype(int)],
+            'ind2': ['Death Rate', df2['death_rate'].astype(int)],
+            'ind3': ['Recovery Rate', df2['recovery_rate'].astype(int)],
+            'ind4': ['Cases per Million Population', df2['cases_per_million'].astype(int)]
+            }
+
+
+    domain_dict = {
+        0: {'x': [0, 0.5], 'y': [0, 0.20]},
+        1: {'x': [0, 0.5], 'y': [0.40, 0.60]},
+        2: {'x': [0.4, 0], 'y': [0, 0.20]},
+        3: {'x': [0.4, 0], 'y': [0.40, 0.60]}
+        }
+    
+    for i in data:
+        fig.add_trace(go.Indicator(
+            mode = "gauge+number",
+            value = data[i][1],
+            title = {'text': data[i][0]},
+            domain = domain_dict[loop]
+        ))
+        loop = loop + 1
+    
+    return(fig)
